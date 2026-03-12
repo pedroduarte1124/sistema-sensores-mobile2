84a0459caf0cabc09373c8c8bb4b8cbe00017757
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-12 20:57:32 -0300

    adaptação

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+type StatusMedicao = "normal" | "alerta" | "critico";
+type Medicao = {
+  id: number;
+  sensor: string;
+  tipo: string;
+  valor: number;
+  status: StatusMedicao;
+};
 export default function App() {
+  const [medicao, setMedicao] = useState<Medicao>({
+    id: 1,
+    sensor: "Motor A - Linha 1",
+    tipo: "TEMPERATURA",
+    valor: 72,
+    status: "normal",
+  });
+  function simularNovaMedicao() {
+    const novoValor = Math.floor(Math.random() * 120);
+    let novoStatus: StatusMedicao = "normal";
+    if (novoValor > 80) {
+      novoStatus = "alerta";
+    }
+    if (novoValor > 100) {
+      novoStatus = "critico";
+    }
+    setMedicao({
+      ...medicao,
+      valor: novoValor,
+      status: novoStatus,
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>
+        Sistema de Monitoramento de Sensores
+      </Text>
+      <View style={styles.card}>
+        <Text>Sensor: {medicao.sensor}</Text>
+        <Text>Tipo: {medicao.tipo}</Text>
+        <Text>Valor: {medicao.valor}</Text>
+        <Text>Status: {medicao.status}</Text>
+        <Button
+          title="Simular Nova Medição"
+          onPress={simularNovaMedicao}
+        />
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#5f65b3",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  card: {
+    width: "80%",
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 8,
   },
 });
